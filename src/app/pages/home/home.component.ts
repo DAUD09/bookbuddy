0a91b50861da2f969ce692ff1b0b4686a532e71f
DAUD09 <rizler09@gmail.com>
2025-08-09 22:53:41 +0200

    book buddy, search feature implementantion

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ CommonModule, HttpClientModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  books: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.searchBooks('harry potter');
+  }
+
+  searchBooks(query: string) {
+    const apiUrl = '`https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(query)}';
+    this.http.get<any>(apiUrl).subscribe({
+      next: (data) => {
+        this.books = data.items || [];
+      },
+      error: (err) => {
+        console.error('Error fetching books', err);
+      }
+    });
+  }
+
+}
