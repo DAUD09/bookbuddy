ae891be9131bb89ed90cd4482f0b4c06364faf40
DAUD09 <rizler09@gmail.com>
2025-08-26 14:51:05 +0200

    configure firebase

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+
+export const environment = {
+    productiton: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyAeXJZSe6S_DoMQr0c70SGlIDAiV-RjoY8",
+        authDomain: "bookbuddy-67aad.firebaseapp.com",
+        projectId: "bookbuddy-67aad",
+        storageBucket: "bookbuddy-67aad.firebasestorage.app",
+        messagingSenderId: "946975402609",
+        appId: "1:946975402609:web:a0fa3bf842d9c987c2c9d0"
+    }     
+};
