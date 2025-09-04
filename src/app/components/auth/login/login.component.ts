b25c9bf611e3d93e21769013f5982a787d962629
DAUD09 <rizler09@gmail.com>
2025-09-04 14:58:58 +0200

    auth

---
+++ src/app/components/auth/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  email = '';
+  password = '';
+
+  constructor(private authService: AuthService) {}
+
+  onLogin() {
+    this.authService.login(this.email, this.password)
+      .then(() => console.log('Logged in!'))
+      .catch(err => console.error(err));
+  }
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, user, User } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  user$: Observable<User | null>;
+
+  constructor(private auth: Auth) {
+    this.user$ = user(this.auth); // keeps tracl of logged-in user
+  }
+
+  signup(email: string, password: string){
+    return createUserWithEmailAndPassword(this.auth, email, password);
+  }
+
+  login(email: string, password: string) {
+    return signInWithEmailAndPassword(this.auth, email, password);
+  }
+
+  logout() {
+    return signOut(this.auth);
+  }
+  
+}
