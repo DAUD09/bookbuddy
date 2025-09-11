b9ff7f0f1b52b4f7f309298c8bcc20ce2060f23a
DAUD09 <rizler09@gmail.com>
2025-09-11 10:18:58 +0200

    signup components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { LoginComponent } from './components/auth/login/login.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'about', component: AboutComponent},
-    {path: 'favorites', component: FavoritesComponent}
+    {path: 'favorites', component: FavoritesComponent},
+    {path: 'signup', component:SignupComponent },
+    {path: 'login', component: LoginComponent }
 ];
--- src/app/components/auth/signup/signup.component.ts
+++ src/app/components/auth/signup/signup.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+  successMessage: string = '';
 
+  constructor(private authService: AuthService) {}
+
+  async onSignup() {
+    this.errorMessage = '';
+    this.successMessage = '';
+    try {
+      await this.authService.signup(this.email, this.password);
+      this.successMessage = 'Signup successful! 🎉 You can now log in,';
+      this.email = '';
+      this.password = '';
+    } catch (err: any) {
+      this.errorMessage = err.errorMessage;
+    }
+  }
 }
